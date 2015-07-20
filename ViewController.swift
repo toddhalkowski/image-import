1a7a04e9e5c2d45210c73dabd8e335a095cee044
Todd Halkowski <thalkowski@virtusa.com>
2015-07-20 13:16:50 -0500

    UIActivityIndicatorView and createAlert additions

--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,46 @@ import Parse
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        // stops app from allowing events during pause function
+    }
+    
+    
+    @IBAction func restore(sender: AnyObject) {
+        
+        activityIndicator.stopAnimating()
+     //   UIApplication.sharedApplication().endIgnoringInteractionEvents()
+    }
+    
+    
+    @IBAction func createAlert(sender: AnyObject) {
+        
+    var alert = UIAlertController(title: "Hey There", message: "Are you sure?", preferredStyle: UIAlertControllerStyle.Alert)
+    
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+            
+        }))
+        
+       self.presentViewController(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
         
         println("Image Selected")
